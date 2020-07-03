1ba780498dbefc8d4b4e12c057e0e485bb6b2c62
Sha Mwe La <shamwela2002@gmail.com>
2020-07-03 22:48:11 +0630

    add code filess

---
+++ app.js
@@ -0,0 +1,44 @@
+const moonPath =
+  "M16.5 27.5C16.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 16.5 12.3122 16.5 27.5Z";
+
+const sunPath =
+  "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+
+const darkMode = document.querySelector("#dark-mode");
+let toggle = false;
+
+darkMode.addEventListener("click", () => {
+  //WE NEED TO USE ANIME.JS
+  //HERE WE SETUP THE TIMELINE
+  const timeline = anime.timeline({
+    duration: 750,
+    easing: "easeInOutQuad",
+  });
+
+  //ADD DIFFERENT ANIMATIONS TO THE TIMELINE
+  timeline
+    .add({
+      targets: ".sun",
+      d: [{ value: toggle ? sunPath : moonPath }],
+    })
+    .add(
+      {
+        targets: "#dark-mode",
+        rotate: 320,
+      },
+      "-= 300"
+    )
+    .add(
+      {
+        targets: "section",
+        //default will be white.
+        backgroundColor: toggle ? "rgb(255,255,255)" : "rgb(0,0,0)",
+        //default will be black.
+        color: toggle ? "rgb(0,0,0)" : "rgb(255,255,255)",
+      },
+      "-= 700"
+    );
+
+  //I WANT TO SWITCH
+  toggle = !toggle;
+});
